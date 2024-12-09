b669c125b27a6ed3b417e210ce0d1cfcb9026bf1
aelithron <aelithron@gmail.com>
2024-12-09 04:28:34 -0700

    Add linting ESLint

--- __tests__/getplayer.test.js
+++ __tests__/getplayer.test.js
@@ -1,4 +1,4 @@
-import { describe, expect, test } from '@jest/globals';
+import { describe, expect } from '@jest/globals';
 import supertest from 'supertest';
 import { app, server } from '../src/app.ts';
 
