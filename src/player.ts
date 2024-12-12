f8b5158d422221a4758c22f7c5c1fd62684602a6
Aelithron <aelithron@gmail.com>
2024-12-12 09:42:27 -0700

    Update player.ts

--- src/player.ts
+++ src/player.ts
@@ -6,7 +6,7 @@ export const router = express.Router();
 export type UserStats = { statID: string, value: string, timestamp: Date, public: boolean }[];
 export type UserError = { error: string, message: string }
 
-router.get('/:id', (req: Request, res: Response) => {
+router.get('/:id', async (req: Request, res: Response) => {
   const player: string = req.params.id
   res.contentType("application/json");
   if (!uuidValidate(player)) {
@@ -14,5 +14,6 @@ router.get('/:id', (req: Request, res: Response) => {
     res.json({ error: "INVALID_UUID", message: "An invalid UUID was provided. Be sure the UUID contains dashes." });
     return;
   }
+  const stats: UserStats | UserError: await readStats
   res.json({ uuid: escapeHtml(player) });
 })
